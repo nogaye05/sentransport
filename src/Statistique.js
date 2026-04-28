95bf4faefece7ba1ca6d3a13497ea5ca7b0984fa
nogaye05 <synogaye70@gmail.com>
2026-04-28 23:30:48 +0000

    Lab 2 : composants statiques LigneBus et ListeLignes

---
+++ src/Statistique.js
@@ -0,0 +1,12 @@
+import './Statistique.css';
+function Statistique() {
+    return(
+      <div className='Statistique'>
+        <span className='chiffre'>10 </span>
+        <span className='libelle'>arret </span>
+
+    </div>
+    );
+    
+}
+export default Statistique;
---
+++ src/Statistique1.js
@@ -0,0 +1,21 @@
+import './Statistique.css';
+
+function Statistique1() {
+  return (
+    <div className="statistique">
+      <span className="statistique-chiffre">10</span>
+      <span className="statistique-libelle">lignes</span>
+    </div>
+  );
+}
+
+export default Statistique1;
+
+
+
+
+
+
+
+
+
